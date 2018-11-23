091ced972fa68125bf0f0a82d4e2f1c62193690b
Michał Nowakowski <michal@localhost.localdomain>
2018-11-23 22:45:09 +0100

    renamed file and created Views

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,20 +2,60 @@ import React from 'react';
 import { 
     View, 
     StyleSheet, 
-    Image,
-    StatusBar
+    ScrollView,
+    Text
 } from 'react-native';
 
-export default class secondScreen extends React.Component{
+export default class HomeScreen extends React.Component{
     static navigationOptions = {
         header: null,
         title: 'Login'
       }
     render() {
         return (
-          <View>
-              <Text>ELO</Text>
+          <View style={styles.container}>
+            <ScrollView contentContainerStyle={{flexGrow : 1, justifyContent : 'center'}} style={styles.contentContainer}>
+                    <Text style={{fontSize: 40}}>gfdgfd</Text>
+                    <Text style={{fontSize: 40}}>gfdgfd</Text>
+                    <Text style={{fontSize: 40}}>gfdgfd</Text>
+                    <Text style={{fontSize: 40}}>gfdgfd</Text>
+                    <Text style={{fontSize: 40}}>gfdgfd</Text>
+                    <Text style={{fontSize: 40}}>gfdgfd</Text>
+                    <Text style={{fontSize: 40}}>gfdgfd</Text>
+                    <Text style={{fontSize: 40}}>gfdgfd</Text>
+                    <Text style={{fontSize: 40}}>gfdgfd</Text>
+                    <Text style={{fontSize: 40}}>gfdgfd</Text>
+                    <Text style={{fontSize: 40}}>gfdgfd</Text>
+                    <Text style={{fontSize: 40}}>gfdgfd</Text>
+                    <Text style={{fontSize: 40}}>gfdgfd</Text>
+                    <Text style={{fontSize: 40}}>gfdgfd</Text>
+                    <Text style={{fontSize: 40}}>gfdgfd</Text>
+                    <Text style={{fontSize: 40}}>gfdgfd</Text>
+                    <Text style={{fontSize: 40}}>gfdgfd</Text>
+                   
+                
+            </ScrollView>
+            <View style={styles.bottomContainer}>
+                  
+            </View>
+            
+              
           </View>
         );
       }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white'
+    },
+    contentContainer: {
+        paddingVertical: 20,
+    },
+    bottomContainer:{
+      width:200,
+      height: 100,
+    }
+
+})
