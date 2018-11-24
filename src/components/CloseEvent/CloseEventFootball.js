bb9784cf43aaba1b56ef7604e8e711ab1b1787e6
Michał Nowakowski <michal951@poczta.onet.pl>
2018-11-24 19:42:11 +0100

    Created new component

---
+++ src/components/CloseEvent/CloseEventFootball.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { 
+    View, 
+    StyleSheet, 
+    Image,
+    StatusBar,
+    Linking,
+    Text,
+} from 'react-native'; 
+
+export default class CloseEventFootball extends React.Component{
+    render(){
+        return(
+            <View style={styles.container}>
+
+            </View>      
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+
+})
