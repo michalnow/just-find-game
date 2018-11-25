a5a635c26fa282c666cceef7e66c38818fd2c8c3
Michał Nowakowski <michal951@poczta.onet.pl>
2018-11-25 20:57:55 +0100

    added bottom tab navigator

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -6,38 +6,51 @@ import {
     Text
 } from 'react-native';
 
+import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs'
+import SettingsScreen from './SettingsScreen'
+
+
 import CloseEventFootball from '../components/CloseEvent/CloseEventFootball'
 
-export default class HomeScreen extends React.Component{
+class HomeScreen extends React.Component{
     static navigationOptions = {
         header: null,
-        title: 'Login'
+        title: 'Home'
       }
     render() {
         return (
           <View style={styles.container}>
             <ScrollView contentContainerStyle={{justifyContent : 'center'}} style={styles.contentContainer}>
-                   <CloseEventFootball />
-                   <CloseEventFootball />
-                   <CloseEventFootball />
-                   <CloseEventFootball />
-                   <CloseEventFootball />
-                   <CloseEventFootball />
+                   <Text>HOME</Text>
             </ScrollView>
             <View style={styles.bottomContainer}>
                   
-            </View>
-            
-              
+            </View>              
           </View>
         );
       }
 }
 
+export default createMaterialBottomTabNavigator({
+    Home: {
+        screen: HomeScreen
+    },
+    Settings: {
+        screen: SettingsScreen
+    }    
+},
+{
+    initialRouteName: 'Home',
+    order:['Settings',"Home"],
+    activeTintColor: '#74b9ff'
+})
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'white'
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center'
     },
     contentContainer: {
         paddingVertical: 20,
