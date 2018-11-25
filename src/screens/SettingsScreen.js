925bbff847923f5a9f4e374942121296de984ab2
Michał Nowakowski <michal951@poczta.onet.pl>
2018-11-25 20:58:19 +0100

    created new screen

---
+++ src/screens/SettingsScreen.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { 
+    View,
+    Text,
+    StyleSheet
+} from 'react-native';
+
+
+export default class SettingsScreen extends React.Component{
+
+    render() {
+        return (
+          <View style={styles.container}>
+            <Text>SETTIGNGS</Text>     
+          </View>
+        );
+      }
+}
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      backgroundColor: 'white',
+      alignItems: 'center',
+      justifyContent: 'center'
+  }
+})
