c07eb32fc7417e9be315ccc0c4785648a4b4a9f8
Michał Nowakowski <michal951@poczta.onet.pl>
2019-01-22 21:03:03 +0100

    refactored component to display volleyball events

--- src/components/CloseEvent/CloseEventVolleyball.js
+++ src/components/CloseEvent/CloseEventVolleyball.js
@@ -1,14 +1,19 @@
 import React from "react";
-import { View, StyleSheet, Image } from "react-native";
+import { View, StyleSheet, ImageBackground, Text } from "react-native";
 
 export default class CloseEventVolleyball extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Image
+        <ImageBackground
           style={styles.image}
-          source={require("../../images/football1.png")}
-        />
+          source={require("../../images/voll.jpg")}
+        >
+          <Text style={styles.text}>Missing: 3</Text>
+          <Text style={styles.text}>Level: Hard</Text>
+          <Text style={styles.text}>10 pm. 24.01</Text>
+          <Text style={styles.text}>ul. Wielicka 10</Text>
+        </ImageBackground>
       </View>
     );
   }
@@ -19,12 +24,21 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    marginTop: 15,
-    marginBottom: 15
+    marginTop: 0,
+    marginBottom: 0,
+    marginRight: 18,
+    marginLeft: 15
   },
   image: {
     width: 300,
-    height: 210,
-    opacity: 0.4
+    height: 200,
+    opacity: 0.8,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 30
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold"
   }
 });
